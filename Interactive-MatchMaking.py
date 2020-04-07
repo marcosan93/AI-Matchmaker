b6694df98ebc2032b2c816aceabaf3bea924fe49
Marco Santos <marcosan93@gmail.com>
2020-04-07 12:45:14 -0400

    created some helper function

--- Interactive-MatchMaking.py
+++ Interactive-MatchMaking.py
@@ -9,11 +9,14 @@ import streamlit as st
 import _pickle as pickle
 
 # Loading the Profiles
-with open("profiles.pkl",'rb') as fp:
+with open("clustered_profiles.pkl",'rb') as fp:
     df = pickle.load(fp)
     
     
+    
+    
 ## Fitting the Vectorizer and Scaler to the Data
+X = df.drop(["Cluster #"], 1)
 
 # Instantiating the Vectorizer
 vectorizer = CountVectorizer()
@@ -40,7 +43,8 @@ scaler.fit(X)
 
 def prep_new_data(new_df):
     """
-    Scales and vectorizes the new data by using the previously fitted scaler and vectorizer
+    Scales and vectorizes the new data by using the previously fitted scaler and vectorizer then 
+    returns that new DF
     """
     # Vectorizing the new data
     vect_new_prof = vectorizer.transform(new_df['Bios'])
@@ -53,5 +57,40 @@ def prep_new_data(new_df):
 
     # Scaling the new profile data
     new_vect_prof = pd.DataFrame(scaler.transform(new_vect_prof), columns=new_vect_prof.columns, index=new_vect_prof.index)
+    
+    return new_vect_prof
+
+
+def top_ten(cluster, new_profile):
+    """
+    Returns the DataFrame containing the top 10 similar profiles to the new data
+    """
+    # Filtering out the clustered DF
+    des_cluster = cluster_df[cluster_df['Cluster #']==designated_cluster[0]]
+    
+    # Appending the new profile data
+    des_cluster = des_cluster.append(new_profile, sort=False)
+
+    # Fitting the vectorizer to the Bios
+    cluster_x = vectorizer.fit_transform(des_cluster['Bios'])
+
+    # Creating a new DF that contains the vectorized words
+    cluster_v = pd.DataFrame(cluster_x.toarray(), index=des_cluster.index, columns=vectorizer.get_feature_names())
+
+    # Joining the Vectorized DF to the previous DF and dropping columns
+    des_cluster = des_cluster.join(cluster_v).drop(['Bios', 'Cluster #'], axis=1)
+    
+    # Trasnposing the DF so that we are correlating with the index(users) and finding the correlation
+    corr = des_cluster.T.corr()
+
+    # Finding the Top 10 similar or correlated users to the new user
+    user_n = new_profile.index[0]
+
+    # Creating a DF with the Top 10 most similar profiles
+    top_10_sim = corr[[user_n]].sort_values(by=[user_n],axis=0, ascending=False)[1:11]
+    
+    return df.drop('Cluster #', 1).loc[top_10_sim.index]
+
+
 
 
