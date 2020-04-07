62077e48129c1391ce2ce6788bdd10064273dc1a
Marco Santos <marcosan93@gmail.com>
2020-04-07 12:38:14 -0400

    started the scaling and vectorizing

--- Interactive-MatchMaking.py
+++ Interactive-MatchMaking.py
@@ -34,3 +34,24 @@ X.drop(['Bios'], axis=1, inplace=True)
 scaler = MinMaxScaler()
 
 scaler.fit(X)
+
+
+## Helper Functions
+
+def prep_new_data(new_df):
+    """
+    Scales and vectorizes the new data by using the previously fitted scaler and vectorizer
+    """
+    # Vectorizing the new data
+    vect_new_prof = vectorizer.transform(new_df['Bios'])
+
+    # Quick DF of the vectorized words
+    new_vect_w = pd.DataFrame(vect_new_prof.toarray(), columns=vectorizer.get_feature_names(), index=new_profile.index)
+
+    # Concatenating the DFs for the new profile data
+    new_vect_prof = pd.concat([new_profile, new_vect_w], 1).drop('Bios', 1)
+
+    # Scaling the new profile data
+    new_vect_prof = pd.DataFrame(scaler.transform(new_vect_prof), columns=new_vect_prof.columns, index=new_vect_prof.index)
+
+
