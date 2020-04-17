407859602bcb05ccac25cac1c96ec7a172953497
Marco Santos <marcosan93@gmail.com>
2020-04-17 19:29:06 -0400

    removed matplotlib from the app file

--- AI-Dating-App/MatchMaking-App.py
+++ AI-Dating-App/MatchMaking-App.py
@@ -2,7 +2,6 @@
 from joblib import load
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import MinMaxScaler
 import streamlit as st
